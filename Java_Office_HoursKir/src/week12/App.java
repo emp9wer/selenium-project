b2d1ab7540ea0a0cbf9ef4ded8f4973a53992138
emp9wer <119460075+emp9wer@users.noreply.github.com>
2023-04-26 11:43:16 -0400

    created Instagram class, app class filled

--- Java_Office_HoursKir/src/week12/App.java
+++ Java_Office_HoursKir/src/week12/App.java
@@ -2,7 +2,37 @@ package week12;
 
 public class App {
 
+private String name;
+private double version;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getVersion() {
+        return version;
+    }
+
+    public void setVersion(double version) {
+        this.version = version;
+    }
 
 
+    public App(String name, double version) {
+        setName(name);
+        setVersion(version);
+    }
 
+
+    @Override
+    public String toString() {
+        return "App{" +
+                "name='" + name + '\'' +
+                ", version=" + version +
+                '}';
+    }
 }
---
+++ Java_Office_HoursKir/src/week12/morning/Instagram.java
@@ -0,0 +1,14 @@
+package week12.morning;
+
+import week12.App;
+
+public class Instagram extends App {
+
+
+    public Instagram(String name, double version) {
+        super(name, version);
+    }
+
+
+
+}
